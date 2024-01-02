eefdfa3be0dc1ba4fca679d39407b28cf8cd55ef
Luka Wanyama <lukewanyama5@gmail.com>
2024-01-02 21:23:17 +0300

    corrections coming up

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(name) {
+    return drivers.filter(driver => driver === name);
+    //driver is a temporary variable that reps each element of the driver.
+}
+let filterDrivers = findMatching('Bobby');
+console.log(filterDrivers);
