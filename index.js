723ae9de0a70590cf799cdf09e1f6768d53f886b
Luka Wanyama <lukewanyama5@gmail.com>
2024-01-03 15:30:29 +0300

    adhuc faciens id

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+//let drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
 
-function findMatching (drivers, name) {
-    let sameDriver = drivers.filter((findHere) => {
-        return findHere.toLowerCase() === name.toLowerCase()
-    })
-    return sameDriver
+function findMatching(drivers, name) {
+  let sameDriver = drivers.filter((findHere) => {
+    return findHere.toLowerCase() === name.toLowerCase();
+  });
+  return sameDriver;
+}
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function matchName(drivers, name) {
+  let newDriver = drivers.filter((sameDriver) => {
+    return sameDriver.name === "Bobby";
+  });
+  return newDriver;
 }
